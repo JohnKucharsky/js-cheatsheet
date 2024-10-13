bbe619d00ea9e939ca2af706e6c05349d2b21e95
vannok <vannok@live.com>
2024-10-13 12:46:22 +0300

    more content added

---
+++ src/app/various2/page.tsx
@@ -0,0 +1,20 @@
+import Content from "./content.mdx";
+import { Metadata } from "next";
+import * as React from "react";
+import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: "Various2",
+};
+
+export default function Page() {
+  return (
+    <>
+      <Content />
+      <div className={"flex justify-between my-4"}>
+        <Link href={"/"}>menu</Link>
+        <Link href={"/leetcode"}>leetcode</Link>
+      </div>
+    </>
+  );
+}
