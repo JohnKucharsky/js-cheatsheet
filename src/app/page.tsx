78fe76d3f74e4da9ae42a2742633b6f8224eb316
ivan <vannok@live.com>
2024-10-19 14:23:25 +0300

    google auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,23 @@
-import Content from "./content.mdx";
+import Menu from "./menu.mdx";
 import { Metadata } from "next";
+import { signIn } from "@/auth";
 
 export const metadata: Metadata = {
   title: "JS Cheatsheet",
 };
 
 export default function Page() {
-  return <Content />;
+  return (
+    <>
+      <form
+        action={async () => {
+          "use server";
+          await signIn("google");
+        }}
+      >
+        <button type="submit">Signin with Google</button>
+      </form>
+      <Menu />
+    </>
+  );
 }
