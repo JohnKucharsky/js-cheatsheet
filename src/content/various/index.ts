001656b3a237c7fd4f4a359a9bb7514cff7b8072
ivan <vannok@live.com>
2024-10-26 20:54:34 +0300

    reducer, redux, useState

--- src/content/various/index.ts
+++ src/content/various/index.ts
@@ -2,14 +2,20 @@ import callStackAndHasAllItems from "./call-stack-and-has-all-items.mdx";
 import mapJs from "./map-js.mdx";
 import priorityQueue from "./priority-queue.mdx";
 import queue from "./queue.mdx";
+import reducer from "./reducer.mdx";
+import redux from "./redux.mdx";
 import setPlus from "./set-plus.mdx";
 import stackAndPalindrome from "./stack-and-palindrome.mdx";
+import useState from "./use-state.mdx";
 
 export const various = [
   callStackAndHasAllItems,
   mapJs,
   priorityQueue,
+  reducer,
+  redux,
   queue,
   setPlus,
   stackAndPalindrome,
+  useState,
 ];
