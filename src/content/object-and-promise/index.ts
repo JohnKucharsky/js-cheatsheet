69cd9c6d0d8d21b8e9adf0963526ec4e392fb596
vannok <vannok@live.com>
2024-11-03 22:07:43 +0300

    promises with objects, event loop started, fixes

--- src/content/object-and-promise/index.ts
+++ src/content/object-and-promise/index.ts
@@ -1,3 +1,4 @@
+import eventLoop from "./event-loop.mdx";
 import objectAssign from "./object-assign.mdx";
 import objectGroupBy from "./object-group-by.mdx";
 import promiseAll from "./promise-all.mdx";
@@ -6,6 +7,7 @@ import promiseAny from "./promise-any.mdx";
 import promiseRace from "./promise-race.mdx";
 
 export const objectAndPromise = [
+  eventLoop,
   objectAssign,
   objectGroupBy,
   promiseAll,
--- src/content/various2/index.ts
+++ src/content/various2/index.ts
@@ -7,6 +7,8 @@ import insertionSort from "./insertion-sort.mdx";
 import isPowerOfTwo from "./is-power-of-two.mdx";
 import pickAndShuffle from "./pick-and-shuffle.mdx";
 import primeNumber from "./prime-number.mdx";
+import promiseAllObject from "./promise-all-object.mdx";
+import promiseAllSettledObject from "./promise-all-settled-object.mdx";
 import quickSort from "./quick-sort.mdx";
 import tsDecorators from "./ts-decorators.mdx";
 
@@ -20,6 +22,8 @@ export const various2 = [
   isPowerOfTwo,
   pickAndShuffle,
   primeNumber,
+  promiseAllObject,
+  promiseAllSettledObject,
   quickSort,
   tsDecorators,
 ];
