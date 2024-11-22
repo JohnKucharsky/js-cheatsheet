d0aaa4e573c6d138a5c38444f8266157f9c7a960
ivan <vannok@live.com>
2024-11-22 19:52:25 +0300

    debug vercel

--- src/app/pick-random/page.tsx
+++ src/app/pick-random/page.tsx
@@ -6,9 +6,11 @@ export const metadata: Metadata = {
 };
 
 export default async function Page() {
+  console.log("page");
   const res = await getFirstItem();
-
+  console.log({ res }, "page");
   if (res[0]?.problem_name === undefined || res[0]?.problem_name === null) {
     return <h4>array is empty, shuffle again</h4>;
   }
+  return <>something went wrong</>;
 }
