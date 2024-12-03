6acaffe1f68649e0072311843ec2f7d17a3c2508
ivan <vannok@live.com>
2024-12-03 10:48:47 +0300

    google tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,12 +3,13 @@
 import "@/app/globals.css";
 import { ReactNode } from "react";
 import * as React from "react";
-import { GoogleAnalytics } from "@next/third-parties/google";
+import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <GoogleAnalytics gaId={"G-KWQLDVETTW"} />
+      <GoogleTagManager gtmId={"GTM-P4PTVL7P"} />
       <body>
         <main>{children}</main>
       </body>
