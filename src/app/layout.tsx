4a83d0e95edbb37d0304bbde0760f87e59ec64d3
ivan <vannok@live.com>
2024-12-03 12:15:12 +0300

    vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,10 +4,12 @@ import "@/app/globals.css";
 import { ReactNode } from "react";
 import * as React from "react";
 import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
+      <Analytics />
       <GoogleAnalytics gaId={"G-KWQLDVETTW"} />
       <GoogleTagManager gtmId={"GTM-P4PTVL7P"} />
       <body>
