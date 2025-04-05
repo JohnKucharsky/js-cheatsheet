ad45162e060091f6302649bc406d92f3a8699e58
vannok <vannok@live.com>
2025-04-05 13:02:01 +0300

    seo, graph image added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,30 +4,27 @@ import * as React from "react";
 import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
 import { Analytics } from "@vercel/analytics/react";
 
+const title = "JavaScript Cheatsheets";
+const description =
+  "Quick reference guides for JavaScript, TypeScript, React, and other tools. Stay productive with concise, easy-to-use cheatsheets.";
+const ogTitle = "JS Cheatsheets – Quick Reference Guides";
+const ogDescription =
+  "Cheatsheets for JavaScript, TypeScript, React, and more.";
+
 export const metadata = {
-  title: "JavaScript Cheatsheets",
-  description:
-    "Quick reference guides for JavaScript, TypeScript, React, and other tools. Stay productive with concise, easy-to-use cheatsheets.",
+  title,
+  description,
   openGraph: {
-    title: "JS Cheatsheets – Quick Reference Guides",
-    description: "Cheatsheets for JavaScript, TypeScript, React, and more.",
+    title: ogTitle,
+    description: ogDescription,
     url: "https://js-cheatsheet-v1.vercel.app",
     siteName: "JS Cheatsheets",
-    images: [
-      {
-        url: "https://js-cheatsheet-v1.vercel.app/og-image.png",
-        width: 1024,
-        height: 657,
-        alt: "JS Cheatsheets",
-      },
-    ],
     type: "website",
   },
   twitter: {
     card: "summary_large_image",
-    title: "JS Cheatsheets",
-    description: "Cheatsheets for JavaScript, TypeScript, React, and more.",
-    images: ["https://js-cheatsheet-v1.vercel.app/og-image.png"],
+    title: ogTitle,
+    description: ogDescription,
   },
 };
 
