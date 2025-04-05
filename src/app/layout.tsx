c2184d5e4763067d0601c4a9ae8181907f8f2387
vannok <vannok@live.com>
2025-04-05 12:05:56 +0300

    seo, graph image added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,35 @@
-"use client";
-
 import "@/app/globals.css";
 import { ReactNode } from "react";
 import * as React from "react";
 import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
 import { Analytics } from "@vercel/analytics/react";
+
+export const metadata = {
+  title: "JavaScript Cheatsheets",
+  description:
+    "Quick reference guides for JavaScript, TypeScript, React, and other tools. Stay productive with concise, easy-to-use cheatsheets.",
+  openGraph: {
+    title: "JS Cheatsheets – Quick Reference Guides",
+    description: "Cheatsheets for JavaScript, TypeScript, React, and more.",
+    url: "https://js-cheatsheet-v1.vercel.app",
+    siteName: "JS Cheatsheets",
+    images: [
+      {
+        url: "https://js-cheatsheet-v1.vercel.app/og-image.png",
+        width: 1024,
+        height: 657,
+        alt: "JS Cheatsheets",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "JS Cheatsheets",
+    description: "Cheatsheets for JavaScript, TypeScript, React, and more.",
+    images: ["https://js-cheatsheet-v1.vercel.app/og-image.png"],
+  },
+};
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
